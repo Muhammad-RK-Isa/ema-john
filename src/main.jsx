8a3f020f5661eed3c32cf190d9b2e7fe9c2c1d45
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-03-27 18:13:25 +0600

    started the workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import Shop from './Components/Shop/Shop';
+import './index.css';
 
 ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
   <BrowserRouter>
     <Header />
     <Routes>
-      <Route path='/' index element={ <Home /> }></Route>
+      <Route path='/' element={ <Home /> }>
+        <Route path='/shop' element={ <Shop /> }></Route>
+      </Route>
     </Routes>
   </BrowserRouter>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: [ "./src/**/*.{jsx, js}" ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'accent': "#1C2B35",
+        'btn-primary': "#FF9900",
+        'btn-secondary': "#995C00",
+        'header': "#2A414F"
+      }
+    },
   },
   plugins: [ require( '@tailwindcss/line-clamp' ) ],
 }
